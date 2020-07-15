898562bfc2ba49243e38bc4cba2609362513011a
sasha <alexander.pop.izmail@gmail.com>
2020-07-15 11:28:34 -0500

    add routers, controller, template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+	'/daterange', 
+	'DateRangeController@index'
+);
+
+Route::post(
+	'/daterange/fetch_data', 
+	'DateRangeController@fetch_data'
+)->name('daterange.fetch_data');
